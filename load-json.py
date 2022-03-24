4ab7cdfd7628c334d59d4a4c1e6a5c092843a28b
shehraj123 <shehrajsingh@gmail.com>
2022-03-24 16:55:31 -0600

    Created function for load-json

--- load-json.py
+++ load-json.py
@@ -4,36 +4,39 @@ from pymongo import MongoClient
 import pymongo
 import json
 
-client = MongoClient("localhost", 27012)
+def load(port):
+	client = MongoClient("localhost", port)
 
-db = client["291db"]
+	db = client["291db"]
 
-name_basics = db["name_basics"]
-title_basics = db["title_basics"]
-title_principals = db["title_principals"]
-title_ratings = db["title_ratings"]
+	name_basics = db["name_basics"]
+	title_basics = db["title_basics"]
+	title_principals = db["title_principals"]
+	title_ratings = db["title_ratings"]
 
-name_basics.drop()
-title_basics.drop()
-title_principals.drop()
-title_ratings.drop()
+	name_basics.drop()
+	title_basics.drop()
+	title_principals.drop()
+	title_ratings.drop()
 
-with open("name.basics.json") as f:
-	data = json.load(f)
+	with open("name.basics.json") as f:
+		data = json.load(f)
 
-name_basics.insert_many(data)
+	name_basics.insert_many(data)
 
-with open("title.basics.json") as f:
-	data = json.load(f)
+	with open("title.basics.json") as f:
+		data = json.load(f)
 
-title_basics.insert_many(data)
+	title_basics.insert_many(data)
 
-with open("title.principals.json") as f:
-	data = json.load(f)
+	with open("title.principals.json") as f:
+		data = json.load(f)
 
-title_principals.insert_many(data)
+	title_principals.insert_many(data)
 
-with open("title.ratings.json") as f:
-	data = json.load(f)
+	with open("title.ratings.json") as f:
+		data = json.load(f)
 
-title_ratings.insert_many(data)
+	title_ratings.insert_many(data)
+
+	return db
