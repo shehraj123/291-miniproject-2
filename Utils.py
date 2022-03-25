39145b406264c62222585b09126a9bde286b9ec8
shehraj123 <shehrajsingh@gmail.com>
2022-03-24 18:42:23 -0600

    Make use of functions inside this file

---
+++ Utils.py
@@ -0,0 +1,45 @@
+# This file contains functions useful for clearing screen and printing prompt
+import os
+
+"""
+The function clears the shell for the new screen.
+Arguments:
+None
+"""
+def shellClear():
+        if os.name == 'nt':
+            os.system('cls')
+
+        elif os.name == 'posix':
+            os.system('clear')
+
+
+"""
+The function clears the shell for the new screen and makes it ready for the next prompt.
+Arguments:
+None
+"""
+def printPrompt(header, prompt):
+    shellClear()
+    print(header)
+    print(prompt) 
+
+"""
+The function takes input from the user and validates it along with error handling
+Arguments:
+None
+"""
+def takeOption(num_options):
+    op = [i+1 for i in range(num_options)]
+    i = 0
+    done = False
+    while not done:
+        try:
+            while i not in op:
+                i = int(input("Choose: "))
+            done = True 
+        except:
+            print("\nEnter valid number")
+            i = 0
+
+    return i                
