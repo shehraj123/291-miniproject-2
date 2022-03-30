04849d53e370362d5828cc85facfa6921a1465c4
Gurveer Sohal <gurveersohal433@gmail.com>
2022-03-30 11:50:57 -0600

    clean up add cast

--- add_cast.py
+++ add_cast.py
@@ -1,15 +1,9 @@
 from Utils import *
+from pymongo import MongoClient
 
 def addCast(db):
 
 	# Intialization
-	shellClear()
-	header = "\t\t\tAdd cast\t\t\t"
-	printPrompt(header, "")
-
-	nconst = input("Enter cast/crew member ID: ").strip()
-	tconst = input("Enter title id: ").strip()
-	category = input("Enter category: ").strip()
 
 	titleExists = False
 	nameExists = False
@@ -19,53 +13,69 @@ def addCast(db):
 	title_principals = db["title_principals"]
 
 	# https://stackoverflow.com/questions/25163658/mongodb-return-true-if-document-exists
+	while True:
+		shellClear()
+		header = "\t\t\tAdd cast\t\t\t"
+		printPrompt(header, "")
+		nconst = input("Enter cast/crew member ID: ").strip()
+		tconst = input("Enter title id: ").strip()
+		category = input("Enter category: ").strip()
 
-	if title_basics.count_documents({ "tconst": tconst }, limit = 1) != 0:
-		titleExists = True
-		print("title found")
+		if title_basics.count_documents({ "tconst": tconst }, limit = 1) != 0:
+			titleExists = True
+			print("Title found")
+		else:
+			print("Title not found")
 
-	if name_basics.count_documents({"nconst" : nconst }, limit = 1) != 0:
-		nameExists = True
-		print("name found")
+		if name_basics.count_documents({"nconst" : nconst }, limit = 1) != 0:
+			nameExists = True
+			print("Name found")
+		else:
+			print("Name not found")
 
-	if nameExists and titleExists:
-		# https://www.statology.org/mongodb-max-value/
-		# https://stackoverflow.com/questions/28968660/how-to-convert-a-pymongo-cursor-cursor-into-a-dict
-		cursor = title_principals.find().sort("ordering", -1).limit(1)
-		l = []
-		for doc in cursor:
-			l.append(doc)
-		d = l[0]
+		if nameExists and titleExists:
+			# https://www.statology.org/mongodb-max-value/
+			# https://stackoverflow.com/questions/28968660/how-to-convert-a-pymongo-cursor-cursor-into-a-dict
+			cursor = title_principals.find().sort("ordering", -1).limit(1)
+			l = []
+			for doc in cursor:
+				l.append(doc)
+			d = l[0]
 
-		ordering = int(d["ordering"])
-		ordering = str(ordering + 1)
+			ordering = int(d["ordering"])
+			ordering = str(ordering + 1)
 
-		row = {
-			"tconst": tconst,
-			"ordering": ordering,
-			"nconst": nconst,
-			"category": category,
-			"job": '\\N',
-			"characters": '\\N'
-		}
+			row = {
+				"tconst": tconst,
+				"ordering": ordering,
+				"nconst": nconst,
+				"category": category,
+				"job": '\\N',
+				"characters": '\\N'
+			}
 
-		title_principals.insert_one(row)
+			title_principals.insert_one(row)
 
-		print("inserted")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			print("Success!")
+			break
+		else:
+			print("Invalid input")
+			print("1. Try again")
+			print("2. Exit")
+			again = False
+			while True:
+				choice = takeOption(2)
+				if choice == 1:
+					again = True
+					break
+				elif choice == 2:
+					break
+					
+			if again:
+				continue
+			else:
+				break		
+if __name__ == "__main__":
+    client = MongoClient('localhost', 27012)
+    db = client["291db"]
+    addCast(db)
